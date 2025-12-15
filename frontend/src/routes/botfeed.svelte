8d262bbb7c06c697a2f1fb2b1f5ea7077892fbb6
Twirlie <popadopz@gmail.com>
2025-12-14 20:53:13 -0600

    add icons

--- frontend/src/routes/botfeed.svelte
+++ frontend/src/routes/botfeed.svelte
@@ -51,6 +51,9 @@
   <h1 class="botfeed-header">Bot Feed</h1>
   {#if testing}
   <div class="botfeed-testing-note">
+    <svg xmlns="http://www.w3.org/2000/svg"fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 notification-icon">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+    </svg>
     <strong>Testing Mode Enabled:</strong> Use the buttons below to add placeholder items to the bot feed.
     <button class="botfeed-button-test" on:click={addOnePlaceholderItemToFeed}>
       Add One Placeholder Item
